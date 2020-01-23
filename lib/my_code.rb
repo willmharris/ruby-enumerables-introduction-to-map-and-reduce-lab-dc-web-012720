789287a8f46abc57db1969c7a2c97a429d947418
William Harris <willmharris0@gmail.com>
2020-01-23 11:12:22 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -50,7 +50,7 @@ end
 
 
 
-def reduce_to_total(source_array, starting_point)
+def reduce_to_total(source_array, starting_point = 0)
   grand_total = starting_point
   i = 0 
   while i < source_array.length do
