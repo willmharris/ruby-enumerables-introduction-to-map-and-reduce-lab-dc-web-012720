a0aa0809dd6b6dd82b983a3c429a8bb2af603124
William Harris <willmharris0@gmail.com>
2020-01-23 11:03:07 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -20,3 +20,15 @@ def map_to_no_change(source_array)
   end
   final_array
 end
+
+def map_to_double(source_array)
+  final_array = []
+  i = 0 
+  while i < source_array.length do 
+    current_number = source_array[i]
+    current_number = current_number*2 
+    final_array.push(current_number)
+    i += 1 
+  end
+  final_array
+end
