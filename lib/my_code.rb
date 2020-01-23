6e87f1779e9e47ab446aa766eee6cc87ce26a14e
William Harris <willmharris0@gmail.com>
2020-01-23 10:55:03 +0000

    Automatically backed up by Learn

---
+++ lib/my_code.rb
@@ -0,0 +1,14 @@
+
+
+
+
+def i_hear_a_sick_sound(passengers_sounds)
+  			i = 0  
+  			while i < passengers_sounds.length do 
+    			if (passengers_sounds[i] == "coughing" || passengers_sounds[i] == "sneezing")
+      				return true
+    			end
+    		i += 1
+  			end
+  			return false
+		end
