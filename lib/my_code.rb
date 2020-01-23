ed13ee8352e469f207aa3a000a43eeb870943cec
William Harris <willmharris0@gmail.com>
2020-01-23 11:04:02 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -38,7 +38,7 @@ def map_to_square(source_array)
   i = 0 
   while i < source_array.length do 
     current_number = source_array[i]
-    current_number = -current_number
+    current_number = current_number*current_number
     final_array.push(current_number)
     i += 1 
   end
