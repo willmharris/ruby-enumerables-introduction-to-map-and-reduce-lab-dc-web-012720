f4c23b2d7bc22732892d3c75b9b66bf5c3468e2e
William Harris <willmharris0@gmail.com>
2020-01-23 11:01:25 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -4,7 +4,7 @@ def map_to_negativize(source_array)
   while i < source_array.length do 
     current_number = source_array[i]
     current_number = -current_number
-    current_number << final_array
+    final_array.unshift(current_number)
     i += 1 
   end
   final_array
