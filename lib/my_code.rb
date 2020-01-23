5e5b62a3f4260b0302e263216da6c939baf7513d
William Harris <willmharris0@gmail.com>
2020-01-23 11:02:29 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -15,7 +15,6 @@ def map_to_no_change(source_array)
   i = 0 
   while i < source_array.length do 
     current_number = source_array[i]
-    current_number = -current_number
     final_array.push(current_number)
     i += 1 
   end
