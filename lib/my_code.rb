838608f0261e3c7ed5022ebfe6a7ed8421ad8d16
William Harris <willmharris0@gmail.com>
2020-01-23 11:35:04 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -66,11 +66,3 @@ def reduce_to_all_true(source_array)
   end 
   return true 
 end
-
-def reduce_to_any_true(source_array)
-  i = 0
-  while i < source_array.length do 
-    
-    i += 1
-  end 
-end
