b483a2253d05366a39a8f94237fe9e10a4c4d3fa
William Harris <willmharris0@gmail.com>
2020-01-23 10:58:56 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,20 +1,12 @@
 def map_to_negativize(source_array)
+  final_array = []
   i = 0 
   while i < source_array.length do 
     
     i += 1 
   end
+  final_array
 end
 
 
 
-def i_hear_a_sick_sound(passengers_sounds)
-		i = 0  
-		while i < passengers_sounds.length do 
-			if (passengers_sounds[i] == "coughing" || passengers_sounds[i] == "sneezing")
-  				return true
-			end
-		  i += 1
-		end
-		return false
-end
